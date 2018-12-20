198cddc3b8cf31703a9e75843cd8f2998a96513f
Tim Smyr <godlyfast@gmail.com>
2018-12-20 23:52:47 +0200

    third

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = ''
+const mnemonic = "";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -15,15 +15,21 @@ module.exports = {
     ropsten: {
       // must be a thunk, otherwise truffle commands may hang in CI
       provider: () =>
-        new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"),
-      network_id: '3',
+        new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"
+        ),
+      network_id: "3",
       gas: 4000000
     },
     kovan: {
       provider: () =>
-       new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/6fb50027e87d49e0889b97a5fba78722'),
-       network_id: '*',
-       gas: 4000000
-     }
+        new HDWalletProvider(
+          mnemonic,
+          "https://kovan.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"
+        ),
+      network_id: "*",
+      gas: 4000000
+    }
   }
 };
