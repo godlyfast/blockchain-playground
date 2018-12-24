9eb91fe3bf753789bc502e4e03d115315e7d372f
Tim Smyr <godlyfast@gmail.com>
2018-12-24 23:34:13 +0200

    Updates

---
+++ precache-manifest.8610f3aa281cd4abc9183405272fbe97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d436d5a994c1eda26ff",
+    "url": "/blockchain-playground/static/js/runtime~main.0d436d5a.js"
+  },
+  {
+    "revision": "ba99b55b5cbf41ef7b0b",
+    "url": "/blockchain-playground/static/js/main.ba99b55b.chunk.js"
+  },
+  {
+    "revision": "fc476494ea3701f7c427",
+    "url": "/blockchain-playground/static/js/1.fc476494.chunk.js"
+  },
+  {
+    "revision": "ba99b55b5cbf41ef7b0b",
+    "url": "/blockchain-playground/static/css/main.dcfb9b14.chunk.css"
+  },
+  {
+    "revision": "f362441ab7c312f64d06ae1b1dd3efd8",
+    "url": "/blockchain-playground/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b3fdb79da2dae655014902cc239bd869.js"
+  "/blockchain-playground/precache-manifest.8610f3aa281cd4abc9183405272fbe97.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blockchain-playground/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
