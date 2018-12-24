52a7446e4e328fea4cfc9edb07b983b4c070f494
Tim Smyr <godlyfast@gmail.com>
2018-12-24 23:47:57 +0200

    deploy work

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic = "";
+const mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -19,7 +19,7 @@ module.exports = {
           mnemonic,
           "https://ropsten.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"
         ),
-      network_id: "3",
+      network_id: "*",
       gas: 4000000
     },
     kovan: {
@@ -27,6 +27,15 @@ module.exports = {
         new HDWalletProvider(
           mnemonic,
           "https://kovan.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"
+        ),
+      network_id: "*",
+      gas: 4000000
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/6fb50027e87d49e0889b97a5fba78722"
         ),
       network_id: "*",
       gas: 4000000
